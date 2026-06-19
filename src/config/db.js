3171d728af512b27027fd5d69d60a0d53050ed9d
Tanishka2424 <Taniskauttarkar@gmail.com>
2026-06-19 23:41:13 +0530

    Fixed MongoDB connection handler

--- src/config/db.js
+++ src/config/db.js
@@ -2,21 +2,18 @@ const mongoose = require("mongoose")
 
 
 
-
-
 function connectToDB() {
 
     mongoose.connect(process.env.MONGO_URI)
-    .then(() =>{
-        console.log("Server is Connected to MongoDB")
-    })
-    .catch((err) =>{
-        app.use(express.json()); // Parse JSON bodies        app.use(express.json()); // Parse JSON bodies        console.error("Error connecting to MongoDB:", err && err.message ? err.message : err)
-        console.error(err)
-        process.exit(1)
-    })
-}
+        .then(() => {
+            console.log("server is connected to DB")
+        })
+        .catch(err => {
+            console.log("Error connecting to DB")
+            process.exit(1)
+        })
 
+}
 
 
 module.exports = connectToDB
